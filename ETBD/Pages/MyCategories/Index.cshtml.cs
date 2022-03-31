fab06ef4734d243a0b7a79251faa8dd32e969a7e
FranciscaLoureiro <francisca.loureiro@hotmail.com>
2022-03-31 11:35:36 +0100

    CRUD Pages Formatting.

--- ETBD/Pages/MyCategories/Index.cshtml.cs
+++ ETBD/Pages/MyCategories/Index.cshtml.cs
@@ -1,14 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.EntityFrameworkCore;
-using ETBD.Data.Entities;
-using ETBDApp.Data;
-
-namespace ETBD.Pages.MyCategories
+﻿namespace ETBD.Pages.MyCategories
 {
     public class IndexModel : PageModel
     {
--- ETBD/Pages/MyFoods/Create.cshtml.cs
+++ ETBD/Pages/MyFoods/Create.cshtml.cs
@@ -1,14 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.AspNetCore.Mvc.Rendering;
-using ETBD.Data.Entities;
-using ETBDApp.Data;
-
-namespace ETBD.Pages.MyFoods
+﻿namespace ETBD.Pages.MyFoods
 {
     public class CreateModel : PageModel
     {
