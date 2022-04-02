9ae785835c7ad864f5647bb5905032299530132c
FranciscaLoureiro <francisca.loureiro@hotmail.com>
2022-04-02 15:11:39 +0100

    Add Razor Pages (Favourite List).

---
+++ ETBD/Pages/MyFavouriteList/Delete.cshtml.cs
@@ -0,0 +1,50 @@
+﻿namespace ETBD.Pages.MyFavouriteList;
+
+public class DeleteModel : PageModel
+{
+    private readonly ETBDApp.Data.ApplicationDbContext _context;
+
+    public DeleteModel(ETBDApp.Data.ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    [BindProperty]
+    public FavouriteList FavouriteList { get; set; }
+
+    public async Task<IActionResult> OnGetAsync(int? id)
+    {
+        if (id == null)
+        {
+            return NotFound();
+        }
+
+        FavouriteList = await _context.FavouriteLists
+            .Include(f => f.Food)
+            .Include(f => f.User).FirstOrDefaultAsync(m => m.Id == id);
+
+        if (FavouriteList == null)
+        {
+            return NotFound();
+        }
+        return Page();
+    }
+
+    public async Task<IActionResult> OnPostAsync(int? id)
+    {
+        if (id == null)
+        {
+            return NotFound();
+        }
+
+        FavouriteList = await _context.FavouriteLists.FindAsync(id);
+
+        if (FavouriteList != null)
+        {
+            _context.FavouriteLists.Remove(FavouriteList);
+            await _context.SaveChangesAsync();
+        }
+
+        return RedirectToPage("./Index");
+    }
+}
---
+++ ETBD/Pages/MyFavouriteList/Index.cshtml.cs
@@ -0,0 +1,25 @@
+﻿namespace ETBD.Pages.MyFavouriteList;
+
+public class IndexModel : PageModel
+{
+    private readonly ETBDApp.Data.ApplicationDbContext _context;
+    private readonly UserManager<IdentityUser> _userManager;
+    public string UserId { get; set; }
+
+    public IndexModel(ApplicationDbContext context, UserManager<IdentityUser> userManager)
+    {
+        _context = context;
+        _userManager = userManager;
+    }
+
+    public IList<FavouriteList> FavouriteList { get;set; }
+
+    public async Task OnGetAsync()
+    {
+        UserId = _userManager.GetUserId(User);
+        FavouriteList = await _context.FavouriteLists
+            .Where(x => x.UserId == UserId)
+            .Include(f => f.Food)
+            .Include(f => f.User).ToListAsync();
+    }
+}
