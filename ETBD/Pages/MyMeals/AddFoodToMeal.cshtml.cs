651e5022c7047725115faf9dc57ac422af88510a
FranciscaLoureiro <francisca.loureiro@hotmail.com>
2022-04-06 11:03:30 +0100

    Revert "Add Razor Pages (Meal)."
    This reverts commit 5b5c0f1e0fcdc91aeb0577f06aae896016f1c0f6.

--- ETBD/Pages/MyMeals/AddFoodToMeal.cshtml.cs
+++ ETBD/Pages/MyMeals/AddFoodToMeal.cshtml.cs
@@ -1,39 +1,39 @@
-namespace ETBD.Pages.MyMeals;
-
-public class AddFoodToMealModel : PageModel
+namespace ETBD.Pages.MyMeals
 {
-    private readonly ETBDApp.Data.ApplicationDbContext _context;
-    public SelectList Foods { get; set; }
-    [BindProperty]
-    public int _MealId { get; set; }
-    [BindProperty]
-    public FoodMeal FoodMeal { get; set; }
-    [BindProperty]
-    [DisplayName("Food")]
-    public int SelectedFoodId { get; set; }
-    public Meal Meal { get; set; }
-    public Food Food { get; set; }
-
-    public AddFoodToMealModel(ETBDApp.Data.ApplicationDbContext context)
+    public class AddFoodToMealModel : PageModel
     {
-        _context = context;
-    }
-    public void OnGet(int MealId)
-    {
-        _MealId = MealId;
-        Foods = new SelectList(_context.Foods, "Id", "Name");
-    }
-    public async Task<IActionResult> OnPostAsync(string mealId)
-    {
-        Meal = _context.Meals.Where(c => c.Id == int.Parse(mealId)).FirstOrDefault();
-        Food = _context.Foods.Where(c => c.Id == SelectedFoodId).FirstOrDefault();
+        private readonly ETBDApp.Data.ApplicationDbContext _context;
+        public SelectList Foods { get; set; }
+        [BindProperty]
+        public int _MealId { get; set; }
+        [BindProperty]
+        public FoodMeal FoodMeal { get; set; }
+        [BindProperty]
+        public int SelectedFoodId { get; set; }
+        public Meal Meal { get; set; }
+        public Food Food { get; set; }
 
-        FoodMeal.Food = Food;
-        FoodMeal.Meal = Meal;
+        public AddFoodToMealModel(ETBDApp.Data.ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public void OnGet(int MealId)
+        {
+            _MealId = MealId;
+            Foods = new SelectList(_context.Foods, "Id", "Name");
+        }
+        public async Task<IActionResult> OnPostAsync(string mealId)
+        {
+            Meal = _context.Meals.Where(c => c.Id == int.Parse(mealId)).FirstOrDefault();
+            Food = _context.Foods.Where(c => c.Id == SelectedFoodId).FirstOrDefault();
 
-        _context.FoodMeals.Add(FoodMeal);
-        await _context.SaveChangesAsync();
+            FoodMeal.Food = Food;
+            FoodMeal.Meal = Meal;
 
-        return RedirectToAction("OnGet", new { MealId = _MealId });
+            _context.FoodMeals.Add(FoodMeal);
+            await _context.SaveChangesAsync();
+
+            return RedirectToAction("OnGet", new { MealId = _MealId });
+        }
     }
 }
